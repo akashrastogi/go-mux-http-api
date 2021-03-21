9317c150955f1749eb3f96e0f4d382c3a3843f07
akash-foodpanda <57343803+akash-foodpanda@users.noreply.github.com>
2021-03-21 20:09:44 +0800

    add gorilla/mux

--- post_service.go
+++ post_service.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
     "log"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Printf("Starting server at port 8080\n")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
+	router := mux.NewRouter()
+
+    if err := http.ListenAndServe(":8080", router); err != nil {
         log.Fatal(err)
     }
 }
